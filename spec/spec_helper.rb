d27b5ab3f8aa4a2049324aeefbb723cd227a0a6d
itmammoth <itmammoth@gmail.com>
2017-10-30 18:46:42 +0900

    Bunlder lower version of pry for ruby-1.9.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'pry-rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
