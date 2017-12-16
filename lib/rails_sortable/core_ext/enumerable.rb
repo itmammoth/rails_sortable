1be1e0dc593fbeb3a11be2fb6c0da12c95a7e798
itmammoth <itmammoth@gmail.com>
2017-12-16 20:19:40 +0900

    [#14] Invoke model#sortable_id for DRY

--- lib/rails_sortable/core_ext/enumerable.rb
+++ lib/rails_sortable/core_ext/enumerable.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def each_with_sortable_id(&block)
     raise "Must be called with block!" unless block_given?
-    each { |e| yield e, "#{e.class}_#{e.id}" }
+    each { |e| yield e, e.sortable_id }
   end
 end
