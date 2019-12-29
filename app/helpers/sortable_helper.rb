ee524470f4066d8aeb0fe9ada11bd77a92b31940
itmammoth <itmammoth@gmail.com>
2019-12-29 23:18:00 +0900

    Delete unnecessary spec

--- app/helpers/sortable_helper.rb
+++
@@ -1,7 +0,0 @@
-module SortableHelper
-  def sortable_fetch(models, &block)
-    warn "[DEPRECATION] `sortable_fetch` is deprecated. Please use Enumerable#each_with_sortable_id instead."
-    raise "Must be called with block!" unless block_given?
-    models.each_with_sortable_id(&block)
-  end
-end
