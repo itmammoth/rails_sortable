0a90eddc470d87218b7f77938409e9e48c9de422
Ahrry <gopalahrry@gmail.com>
2021-07-05 10:43:27 +0200

    chore(sort) start index at 1 instead of 0

--- app/controllers/sortable_controller.rb
+++ app/controllers/sortable_controller.rb
@@ -11,9 +11,9 @@ class SortableController < ApplicationController
   #
   def reorder
     ActiveRecord::Base.transaction do
-      params['rails_sortable'].each_with_index do |token, new_sort|
+      params['rails_sortable'].each.with_index(1) do |token, new_sort|
         next unless token.present?
-        
+
         model = find_model(token)
         current_sort = model.read_attribute(model.class.sort_attribute)
         model.update_sort!(new_sort) if current_sort != new_sort
